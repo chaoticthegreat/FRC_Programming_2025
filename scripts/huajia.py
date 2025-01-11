17e349a046b4bf44d3c50f334d720726f64c0901
ThatXliner <bryan.hu.2020@gmail.com>
2025-01-10 17:41:59 -0800

    :sparkles: Generate

--- scripts/huajia.py
+++ scripts/huajia.py
@@ -10,12 +10,23 @@ class Pose:
     x: float
     y: float
     angle: float
+    name: None | str = None
 
     def export(self):
         return {
-            "x": {"exp": f"{self.x} m", "val": self.x},
-            "y": {"exp": f"{self.y} m", "val": self.y},
-            "heading": {"exp": f"{self.angle} rad", "val": self.angle},
+            **(
+                {
+                    "x": {"exp": f"{self.x} m", "val": self.x},
+                    "y": {"exp": f"{self.y} m", "val": self.y},
+                    "heading": {"exp": f"{self.angle} rad", "val": self.angle},
+                }
+                if self.name is None
+                else {
+                    "x": {"exp": f"{self.name}.x", "val": self.x},
+                    "y": {"exp": f"{self.name}.y", "val": self.y},
+                    "heading": {"exp": f"{self.name}.heading", "val": self.angle},
+                }
+            ),
             "intervals": 40,
             "split": False,
             "fixTranslation": True,
@@ -24,15 +35,9 @@ class Pose:
         }
 
 
-c1 = Pose(x=3.518733263015747, y=5.551057815551758, angle=-1.0303767131053394)
-c2 = Pose(x=5.435359954833984, y=5.50984001159668, angle=-2.111216507579087)
-c3 = Pose(x=6.2184977531433105, y=4.14965295791626, angle=-3.1183410182726012)
-c4 = Pose(x=5.414751052856445, y=2.5833775997161865, angle=-2.0303115817332302)
-
-
 def template(waypoints, name="New Path"):
     return {
-        "name": "New Path",
+        "name": name,
         "version": 1,
         "snapshot": {"waypoints": [], "constraints": [], "targetDt": 0.05},
         "params": {
@@ -63,19 +68,47 @@ def template(waypoints, name="New Path"):
     }
 
 
+def load_poses(raw):
+    for pose in raw:
+        yield pose, Pose(
+            x=raw[pose]["x"]["val"],
+            y=raw[pose]["y"]["val"],
+            angle=raw[pose]["heading"]["val"],
+            name=pose,
+        )
+
+
 def main() -> None:
     """Run the app."""
-    poses = [c1, c2, c3, c4]
-    for i in range(1 << len(poses)):
-        Path(f"path_{i}.traj").write_text(
+    poses = dict(
+        load_poses(
+            json.loads(Path("src/main/deploy/choreo/Choreo.chor").read_text())[
+                "variables"
+            ]["poses"]
+        )
+    )
+    for pose in "ABCDEFGHIJK":
+        path = [poses["Source2"], poses[pose]]
+        name = f"{'-'.join(p.name for p in path)}"
+        Path(f"src/main/deploy/choreo/{name}.traj").write_text(
             json.dumps(
                 template(
-                    [pose.export() for j, pose in enumerate(poses) if i & (1 << j)],
-                    f"Path {i}",
+                    [pose.export() for pose in path],
+                    name,
                 ),
             )
         )
-        print(i)
+        print(name)
+    # for i in range(1 << len(poses)):
+    #     Path(f"src/main/choreo/path_{i}.traj").write_text(
+    #         json.dumps(
+    #             template(
+    #                 [pose.export() for j, pose in enumerate(poses) if i & (1 << j)],
+    #                 f"Path {i}",
+    #             ),
+    #         )
+    #     )
+    #     print(i)
 
 
 if __name__ == "__main__":
